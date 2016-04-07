be43aba4625536bc6b431a3a83418bccc64669c3
Helge Stenström <h.stenstrom@gmail.com>
2016-04-07 23:05:03 +0200

    Print bytt mot assert

--- my2.py
+++ my2.py
@@ -19,13 +19,16 @@ class WasRun(TestCase):
     def testMethod(self):
     	self.wasRun = True
 
-   	
-
-
 # =====  Unit tests start here ==========
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+	"""Testar mina test case"""
+	def testRunning(self):
+		test = WasRun("testMethod")
+		assert(not test.wasRun)
+		test.run()
+		assert(test.wasRun)
 
+
+
+TestCaseTest("testRunning").run()

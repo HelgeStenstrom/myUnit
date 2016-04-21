8bcfa7a2072df0fe6b7c225a544b1b9d47b19229
Helge Stenström <h.stenstrom@gmail.com>
2016-04-21 18:38:01 +0200

    Separata tester

--- my2.py
+++ my2.py
@@ -40,63 +40,63 @@ class WasRun(TestCase):
         self.log = "setUp "
 
 
-# =====  Unit tests start here ==========
+# # =====  Unit tests start here ==========
 
-class TestCaseTest(TestCase):
-    """Testar mina test case"""
+# class TestCaseTest(TestCase):
+#     """Testar mina test case"""
 
-    def setUp(self):
-        self.test = WasRun("testMethod")
+#     def setUp(self):
+#         self.test = WasRun("testMethod")
 
-    def testSetUp(self):
-        self.test.run()
-        assert("setUp testMethod " == self.test.log)
+#     def testSetUp(self):
+#         self.test.run()
+#         assert("setUp testMethod " == self.test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
+#     def testRunning(self):
+#         self.test.run()
+#         assert(self.test.wasRun)
 
-    def testFailNoMsg(self):
-        try:
-            self.fail()
-        except AssertionError:
-            pass
-        else:
-            raise AssertionError
+#     def testFailNoMsg(self):
+#         try:
+#             self.fail()
+#         except AssertionError:
+#             pass
+#         else:
+#             raise AssertionError
 
-    def testFailMsg(self):
-        try:
-            self.fail("argument")
-        except AssertionError as e:
-            if e.args[0] != "argument":
-                raise ValueError("fail(arg) hanterar inte argumentet rätt")
-        else:
-            raise AssertionError
-
-
-    def testAssertFaster_tooSlow(self):
-        time.sleep(0.2)
-        try:
-            self.assertFaster(0.1)
-        except TimeoutError:
-            pass
-        else:
-            self.fail("Did not catch too slow execution.")
-
-    def testAssertFaster_notTooSlow(self):
-        time.sleep(0)
-        try:
-            self.assertFaster(1)
-        except TimeoutError:
-            self.fail("Faulty assertion of slow running time")
+#     def testFailMsg(self):
+#         try:
+#             self.fail("argument")
+#         except AssertionError as e:
+#             if e.args[0] != "argument":
+#                 raise ValueError("fail(arg) hanterar inte argumentet rätt")
+#         else:
+#             raise AssertionError
 
 
-# Lista test att köra
+#     def testAssertFaster_tooSlow(self):
+#         time.sleep(0.2)
+#         try:
+#             self.assertFaster(0.1)
+#         except TimeoutError:
+#             pass
+#         else:
+#             self.fail("Did not catch too slow execution.")
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
-TestCaseTest("testFailNoMsg").run()
-TestCaseTest("testFailMsg").run()
-TestCaseTest("testAssertFaster_tooSlow").run()
-TestCaseTest("testAssertFaster_notTooSlow").run()
-print("end of program")
+#     def testAssertFaster_notTooSlow(self):
+#         time.sleep(0)
+#         try:
+#             self.assertFaster(1)
+#         except TimeoutError:
+#             self.fail("Faulty assertion of slow running time")
+
+
+# # Lista test att köra
+
+# TestCaseTest("testRunning").run()
+# TestCaseTest("testSetUp").run()
+# TestCaseTest("testFailNoMsg").run()
+# TestCaseTest("testFailMsg").run()
+# TestCaseTest("testAssertFaster_tooSlow").run()
+# TestCaseTest("testAssertFaster_notTooSlow").run()
+# print("end of program")
---
+++ my2_tests.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*.
+
+# Från Kent Beck
+
+
+import my2 
+import time
+
+
+# =====  Unit tests start here ==========
+
+class TestCaseTest(my2.TestCase):
+    """Testar mina test case"""
+
+    def setUp(self):
+        self.test = my2.WasRun("testMethod")
+
+    def testSetUp(self):
+        self.test.run()
+        assert("setUp testMethod " == self.test.log)
+
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testFailNoMsg(self):
+        try:
+            self.fail()
+        except AssertionError:
+            pass
+        else:
+            raise AssertionError
+
+    def testFailMsg(self):
+        try:
+            self.fail("argument")
+        except AssertionError as e:
+            if e.args[0] != "argument":
+                raise ValueError("fail(arg) hanterar inte argumentet rätt")
+        else:
+            raise AssertionError
+
+
+    def test_AssertFaster_tooSlow(self):
+        time.sleep(0.2)
+        try:
+            self.assertFaster(0.1)
+        except TimeoutError:
+            pass
+        else:
+            self.fail("Did not catch too slow execution.")
+
+    def test_AssertFaster_notTooSlow(self):
+        time.sleep(0)
+        try:
+            self.assertFaster(1)
+        except TimeoutError:
+            self.fail("Faulty assertion of slow running time")
+
+
+    def test_AssertTrue(self):
+        raise Exception
+
+
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
+TestCaseTest("testFailNoMsg").run()
+TestCaseTest("testFailMsg").run()
+TestCaseTest("test_AssertFaster_tooSlow").run()
+TestCaseTest("test_AssertFaster_notTooSlow").run()
+print("end of program")
